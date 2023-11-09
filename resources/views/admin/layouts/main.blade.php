b3e7feeba96661632e6662445fb10c5b512fd4c9
Arnius <sehatkopikap@gmail.com>
2023-11-09 12:50:15 +0700

    Update library

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -9,10 +9,10 @@
         <meta name="author" content="" />
         <title>Affiliate Dashboard - SB Admin Pro</title>
         <link href="{{ asset('templates/sb-admin-pro/cdn.jsdelivr.net/npm/litepicker/dist/css/litepicker.css') }}" rel="stylesheet" />
-        <link href="{{ asset('tempaltes/sb-admin-pro/css/styles.css') }}" rel="stylesheet" />
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.png" />
-        <script data-search-pseudo-elements="" defer="" src="../cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('template/sb-admin-procdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.min.js') }}" crossorigin="anonymous"></script>
+        <link href="{{ asset('templates/sb-admin-pro/css/styles.css') }}" rel="stylesheet" />
+        <link rel="icon" type="image/x-icon" href="{{ asset('templates/sb-admin-pro/assets/img/favicon.png') }}" />
+        <script data-search-pseudo-elements="" defer="" src="{{ asset('templates/sb-admin-pro/cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js') }}" crossorigin="anonymous"></script>
+        <script src="{{ asset('templates/sb-admin-pro/cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.min.js') }}" crossorigin="anonymous"></script>
     </head>
     <body class="nav-fixed">
         @include('admin.partials.topbar')
@@ -29,14 +29,14 @@
         </div>
         <script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="../cdn.jsdelivr.net/npm/bootstrap%405.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
-        <script src="../cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="assets/demo/chart-area-demo.js"></script>
-        <script src="assets/demo/chart-bar-demo.js"></script>
-        <script src="assets/demo/chart-pie-demo.js"></script>
-        <script src="../cdn.jsdelivr.net/npm/litepicker/dist/bundle.js" crossorigin="anonymous"></script>
-        <script src="js/litepicker.js"></script>
+        <script src="{{ asset('templates/sb-admin-pro/cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js') }}" crossorigin="anonymous"></script>
+        <script src="{{ asset('templates/sb-admin-pro/assets/demo/chart-area-demo.js') }}"></script>
+        <script src="{{ asset('templates/sb-admin-pro/assets/demo/chart-bar-demo.js') }}"></script>
+        <script src="{{ asset('templates/sb-admin-pro/assets/demo/chart-pie-demo.js') }}"></script>
+        <script src="{{ asset('templates/sb-admin-pro/cdn.jsdelivr.net/npm/litepicker/dist/bundle.js') }}" crossorigin="anonymous"></script>
+        <script src="{{ aseet('templates/sb-admin-pro/js/litepicker.js') }}"></script>
 
-        <script src="../assets.startbootstrap.com/js/sb-customizer.js"></script>
+        <script src="{{ asset('templates/sb-admin-pro/assets.startbootstrap.com/js/sb-customizer.js') }}"></script>
         <sb-customizer project="sb-admin-pro"></sb-customizer>
     <script>(function(){var js = "window['__CF$cv$params']={r:'822e4aec3e498b2f',t:'MTY5OTQ1MTUwNi43NzAwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='cdn-cgi/challenge-platform/h/g/scripts/jsd/9914b343/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";var _0xh = document.createElement('iframe');_0xh.height = 1;_0xh.width = 1;_0xh.style.position = 'absolute';_0xh.style.top = 0;_0xh.style.left = 0;_0xh.style.border = 'none';_0xh.style.visibility = 'hidden';document.body.appendChild(_0xh);function handler() {var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;if (_0xi) {var _0xj = _0xi.createElement('script');_0xj.innerHTML = js;_0xi.getElementsByTagName('head')[0].appendChild(_0xj);}}if (document.readyState !== 'loading') {handler();} else if (window.addEventListener) {document.addEventListener('DOMContentLoaded', handler);} else {var prev = document.onreadystatechange || function () {};document.onreadystatechange = function (e) {prev(e);if (document.readyState !== 'loading') {document.onreadystatechange = prev;handler();}};}})();</script><script defer src="https://static.cloudflareinsights.com/beacon.min.js/v84a3a4012de94ce1a686ba8c167c359c1696973893317" integrity="sha512-euoFGowhlaLqXsPWQ48qSkBSCFs3DPRyiwVu3FjR96cMPx+Fr+gpWRhIafcHwqwCqWS42RZhIudOvEI+Ckf6MA==" data-cf-beacon='{"rayId":"822e4aec3e498b2f","b":1,"version":"2023.10.0","token":"6e2c2575ac8f44ed824cef7899ba8463"}' crossorigin="anonymous"></script>
 </body>
