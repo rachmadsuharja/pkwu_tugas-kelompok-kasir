b93d0fa5ea6764e9c91af370457dfd0aa339d645
Arnius <sehatkopikap@gmail.com>
2023-11-15 13:57:05 +0700

    Update Sidebar responsive

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -2,23 +2,7 @@
     <nav class="sidenav shadow-right sidenav-light">
         <div class="sidenav-menu">
             <div class="nav accordion" id="accordionSidenav">
-                <!-- Sidenav Menu Heading (Account)-->
-                <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-                <div class="sidenav-menu-heading d-sm-none">Account</div>
-                <!-- Sidenav Link (Alerts)-->
-                <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-                <a class="nav-link d-sm-none" href="#!">
-                    <div class="nav-link-icon"><i data-feather="bell"></i></div>
-                    Alerts
-                    <span class="badge bg-warning-soft text-warning ms-auto">4 New!</span>
-                </a>
-                <!-- Sidenav Link (Messages)-->
-                <!-- * * Note: * * Visible only on and above the sm breakpoint-->
-                <a class="nav-link d-sm-none" href="#!">
-                    <div class="nav-link-icon"><i data-feather="mail"></i></div>
-                    Messages
-                    <span class="badge bg-success-soft text-success ms-auto">2 New!</span>
-                </a>
+
                 <!-- Sidenav Accordion (Dashboard)-->
                 <a class="nav-link mt-3 {{ (Request::routeIs('dashboard') ? 'active' : '') }}" href="{{ route('dashboard') }}">
                     <div class="nav-link-icon"><i data-feather="activity"></i></div>
